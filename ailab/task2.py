02699f1d8df547039563995d75f324d9fb0796ab
2403a51009-del <2403a51009@sru.edu.in>
2025-09-17 14:44:59 +0530

    Add files via upload

--- ailab/task2.py
+++
@@ -1,8 +0,0 @@
-def find_common(a, b):
-    res = []
-    for i in a:
-        for j in b:
-            if i == j:
-                res.append(i)
-    return res 
-print(find_common([1, 2, 3], [2, 3, 4]))  
